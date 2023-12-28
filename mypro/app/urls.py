e06713066c9220b2194f9732c3d49c8ae387711b
avin <avink080@gmail.com>
2023-12-28 17:50:44 +0530

    added

--- mypro/app/urls.py
+++ mypro/app/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     # path('update/<int:id>/',views.update,name="update"),
     # path('delete/<int:id>/',views.delete,name="delete"),
     # path('register/',views.register,name="register"),
-    # path('login/', views.login, name="login"),
+    path('login/', views.login, name="login"),
     # path('logout/', views.logout, name="logout"),
 
 ]
--- mypro/app/views.py
+++ mypro/app/views.py
@@ -57,44 +57,44 @@ def details(request,watch_id):
 
 
 # USER AUTHENTICATION PART
-# def register(request):
-#     if request.method=='POST':
-#         if request.method=='POST':
-#             username=request.POST.get('user_name')
-#             firstname = request.POST.get('first_name')
-#             lastname = request.POST.get('last_name')
-#             email= request.POST.get('email')
-#             pwd = request.POST.get('pwd')
-#             cnfpwd = request.POST.get('cnfpwd')
-#             if pwd==cnfpwd:
-#                 if User.objects.filter(username=username).exists():
-#                     messages.info(request,"Username Already Exist")
-#                     return redirect('/register')
-#                 elif User.objects.filter(email=email).exists():
-#                     messages.info(request, "Email Already Exist")
-#                     return redirect('/register')
-#                 else:
-#                     user=User.objects.create(username=username,first_name=firstname,last_name=lastname,email=email,password=pwd)
-#                     user.save()
-#                 return redirect('/')
-#             else:
-#                 messages.info(request, "Password Not Match")
-#                 return redirect('/register')
-#     return render(request,'register.html')
+def register(request):
+    if request.method=='POST':
+        if request.method=='POST':
+            username=request.POST.get('user_name')
+            firstname = request.POST.get('first_name')
+            lastname = request.POST.get('last_name')
+            email= request.POST.get('email')
+            pwd = request.POST.get('pwd')
+            cnfpwd = request.POST.get('cnfpwd')
+            if pwd==cnfpwd:
+                if User.objects.filter(username=username).exists():
+                    messages.info(request,"Username Already Exist")
+                    return redirect('/register')
+                elif User.objects.filter(email=email).exists():
+                    messages.info(request, "Email Already Exist")
+                    return redirect('/register')
+                else:
+                    user=User.objects.create(username=username,first_name=firstname,last_name=lastname,email=email,password=pwd)
+                    user.save()
+                return redirect('/')
+            else:
+                messages.info(request, "Password Not Match")
+                return redirect('/register')
+    return render(request,'register.html')
 
-# def login(request):
-#     if request.method=='POST':
-#         username=request.POST.get('username')
-#         pwd=request.POST.get('pwd')
-#         user=auth.authenticate(username=username,password=pwd)
-#         if user is not None:
-#             auth.login(request,user)
-#             return redirect('/')
-#         else:
-#             messages.info(request,'Invalid Entry')
-#             return redirect('/login')
+def login(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        pwd=request.POST.get('pwd')
+        user=auth.authenticate(username=username,password=pwd)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,'Invalid Entry')
+            return redirect('/login')
 
-#     return render(request,'login.html')
+    return render(request,'login.html')
 
 # def logout(request):
 #     auth.logout(request)
